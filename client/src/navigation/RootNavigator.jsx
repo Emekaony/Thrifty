17e4123b04af128788326692cd70dcb5368b6e55
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-01-11 01:21:13 -0800

    installed react navigator

---
+++ client/src/navigation/RootNavigator.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import OnBoard from "../components/auth/OnBoard";
+
+const RootNavigator = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+export default RootNavigator;
