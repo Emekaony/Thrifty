4d1ed3f9c2320e3d4f28bdca6610d4b897f1b922
DON <don@dons-laptop.myfiosgateway.com>
2023-01-11 04:15:46 -0500

    minor changes

--- client/App.js
+++ client/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar"
+import { Alert, TouchableOpacity, StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+const Separator = () => <View style={styles.separator} />
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Working</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [count, setCount] = useState(0)
+    const onPress = () => setCount((pC) => pC + 1)
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={onPress}>
+                <Text>Click me</Text>
+            </TouchableOpacity>
+            <Separator />
+            <Text>You clicked me {count} times!</Text>
+            <StatusBar style='auto' />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    separator: {
+        marginVertical: 8,
+    },
+    button: {
+        color: "#841584",
+        backgroundColor: "#DDDDDD",
+        padding: 10,
+        borderRadius: 5,
+    },
+})
--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 module.exports = async () => {
     const databaseParams = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    }
-    mongoose.set("strictQuery", false)
+    };
+    mongoose.set("strictQuery", false);
     try {
-        mongoose.connect(process.env.DB, databaseParams)
-        console.log("connected to the mongodb database")
+        mongoose.connect(process.env.DB, databaseParams);
+        console.log("connected to the mongodb database");
     } catch (error) {
-        console.log(`${error} could not connect`)
+        console.log(`${error} could not connect`);
     }
-}
+};
