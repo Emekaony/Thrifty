b7635fb83e0b14c8889bc2be5e981620ed103db0
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-01-11 23:17:16 -0800

    config is good

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 module.exports = async () => {
     const databaseParams = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    };
-    mongoose.set("strictQuery", false);
-    try {
-        mongoose.connect(process.env.DB, databaseParams);
-        console.log("connected to the mongodb database");
-    } catch (error) {
-        console.log(`${error} could not connect`);
     }
-};
+    mongoose.set("strictQuery", false)
+    try {
+        mongoose.connect(process.env.DB, databaseParams)
+        console.log("connected to the mongodb database")
+    } catch (error) {
+        console.log(`${error} could not connect`)
+    }
+}
